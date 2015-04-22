40c20926893f07b976a3fd938e8130fe376759e0
Sergey Lyah <medion89@gmail.com>
2015-04-22 22:33:43 +0300

    132

--- Source/SharpGame/Program.cs
+++ Source/SharpGame/Program.cs
@@ -4,13 +4,30 @@
     {
         static void Main(string[] args)
         {
-            Actor Raketka = new Actor();
-            ColoredText rakpaint= new ColoredText();
-            
-            Raketka.AddEntity(rakpaint);
+            Actor actor = new Actor();
+            Actor actor2 = new Actor();
 
+            actor.Position = new Vector3(5, 5, 0);
+            actor2.Position = new Vector3(5, 17, 0);
+
+            ColoredText rocket1 = new ColoredText(System.ConsoleColor.Red, System.ConsoleColor.Blue);
+            rocket1.text = new char[,]
+            {{' ','#',' '},
+             {' ','#',' '},
+             {' ','#',' '},
+             {' ','#',' '},
+             {' ','#',' '}};
+
+            ColoredText rocket2 = new ColoredText(System.ConsoleColor.Red, System.ConsoleColor.Blue);
+
+            rocket2.text = new char[,]
+            {{' ','#',' '},
+             {' ','#',' '},
+             {' ','#',' '},
+             {' ','#',' '},
+             {' ','#',' '}};
             Scene scene = new Scene();
-            scene.AddEntity(Raketka);
+            scene.AddEntity(Raketka1);
 
             Game game = new Game();
             game.TargetFPS = 60;
